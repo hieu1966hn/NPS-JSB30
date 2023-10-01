bdfd5a3295e6335bfc765143d6014c5a0027e1db
hieu1966hn <hieu1966hn@gmail.com>
2023-10-01 17:38:57 +0700

    new ss9

--- SS8/app.js
+++ SS8/app.js
@@ -1,1 +1,21 @@
-// let form = 
+let form = document.getElementById("register-form");
+console.log("form: ", form);
+
+
+// Cách để collect dữ liệu người dùng như sau
+form.addEventListener("submit", function (event) {
+  // Ngăn sự kiện reload mặc định của trình duyệt
+  event.preventDefault();
+
+  // console.log("Bạn đã ấn nút đăng ký thành công => chạy vào phạm vi hoạt đọng của hàm này");
+
+  // Lấy ra dữ liệu người dùng nhập vào gồm
+  console.log(
+    form.username.value.trim()
+  );
+
+  console.log(
+    form.email.value.trim()
+  );
+
+});
